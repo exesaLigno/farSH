cc5a5472777a0fa9b7d1e30ed58b7fcbc6ccda02
Vadim Karcev <cfg888powerfull@gmail.com>
2025-10-03 13:07:06 +0300

    UnicodeString rewritten

--- string.cpp
+++ string.cpp
@@ -7,105 +7,116 @@
 class UnicodeString
 {
 private:
-    size_t cursorPosition = 0;
-    size_t displayCursorPosition = 0;
+    static constexpr size_t initialBufferSize = 16;
+
+    UnicodeSymbol* buffer = nullptr;
+    size_t bufferSize = 0;
     size_t length = 0;
-    size_t displayWidth = 0;
+    size_t width = 0;
+
+    void ReallocateBuffer(size_t multiplier = 2, size_t added_size = 0)
+    {
+        size_t old_buffer_size = bufferSize;
+        UnicodeSymbol* old_buffer = buffer;
+
+        bufferSize = bufferSize * multiplier + added_size;
+        buffer = new UnicodeSymbol[bufferSize];
+        memcpy(buffer, old_buffer, old_buffer_size * sizeof(UnicodeSymbol));
+        delete[] old_buffer;
+    }
+
+    void AppendSymbolAndReallocate(const UnicodeSymbol& sym)
+    {
+        if (length >= bufferSize)
+            ReallocateBuffer();
+        buffer[length++] = sym;
+        width += sym.DisplayWidth();
+    }
 
 public:
-    UnicodeString() { }
+    UnicodeString()
+    {
+        ReallocateBuffer(0, initialBufferSize);
+    }
+
     UnicodeString(const char* string)
     {
+        ReallocateBuffer(0, initialBufferSize);
+
         const char* string_ptr = string;
-        
         while (*string_ptr != '\0')
         {
-            UnicodeSymbol symbol = UnicodeSymbol::CreateFromStream([&string_ptr]() {
+            AppendSymbolAndReallocate(UnicodeSymbol::CreateFromStream([&string_ptr]() {
                 return *(string_ptr++);
-            });
+            }));
+        }
+    }
+
+    ~UnicodeString()
+    {
+        if (buffer)
+        {
+            delete[] buffer;
+            buffer = nullptr;
         }
     }
 
     void Clear()
     {
-        cursorPosition = 0;
-        displayCursorPosition = 0;
         length = 0;
-        displayWidth = 0;
-    }
-
-    void SetCursorPosition(size_t new_position)
-    {
-        
-    }
-
-    void SetDisplayCursorPosition(size_t new_position)
-    {
-
-    }
-
-    UnicodeString operator+(const UnicodeString& other) const
-    {
-        UnicodeString result;
-
-        result.length = length + other.length;
-        result.displayWidth = displayWidth + other.displayWidth;
-    }
-
-    UnicodeString operator+(const UnicodeSymbol& other) const
-    {
-
-    }
-
-    UnicodeString operator+(const char* string) const
-    {
-
-    }
-
-    UnicodeString operator+(const char symbol) const
-    {
-
+        width = 0;
     }
 
     UnicodeString& operator+=(const UnicodeString& string)
     {
-        length += string.length;
-        displayWidth += string.displayWidth;
+        for (size_t idx = 0; idx < string.length; idx++)
+            AppendSymbolAndReallocate(string.buffer[idx]);
+
+        return *this;
     }
 
     UnicodeString& operator+=(const UnicodeSymbol& symbol)
     {
-        length += 1;
-        displayWidth += symbol.DisplayWidth();
+        AppendSymbolAndReallocate(symbol);
+        return *this;
     }
 
     UnicodeString& operator+=(const char* string)
     {
-
+        const char* string_ptr = string;
+        while (*string_ptr != '\0')
+        {
+            AppendSymbolAndReallocate(UnicodeSymbol::CreateFromStream([&string_ptr]() {
+                return *(string_ptr++);
+            }));
+        }
+        return *this;
     }
 
     UnicodeString& operator+=(const char symbol)
     {
-
+        AppendSymbolAndReallocate(UnicodeSymbol(symbol));
+        return *this;
     }
 
-    size_t DisplayWidth()
+    UnicodeSymbol& operator[](size_t idx)
     {
-        return displayWidth;
+        return buffer[idx];
     }
 
-    size_t CursorPosition()
+    void WriteTo(FILE* fd = stdout) const
     {
-        return cursorPosition;
+        for (size_t idx = 0; idx < length; idx++)
+            buffer[idx].WriteTo(fd);
     }
 
-    size_t DisplaCursorPosition()
+    size_t Length() const
     {
-        return displayCursorPosition;
+        return length;
     }
 
-    void WriteTo(FILE* stream)
+    size_t Width() const
     {
-
+        return width;
     }
 };
