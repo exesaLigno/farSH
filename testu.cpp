93a356aaa05d05712f7f96eb138acc9a62f583d9
Vadim Karcev <cfg888powerfull@gmail.com>
2025-10-03 13:08:45 +0300

    Testing new string...

--- testu.cpp
+++ testu.cpp
@@ -1,10 +1,23 @@
-#include <wchar.h>
-#include <cstdint>
+#include "string.cpp"
 
 int main()
 {
-	wchar_t c = L'а';
-	uint8_t m[4] = { 0, 0, 0xD0, 0xB0 };
-
-	return c == *((wchar_t*) m);
+	auto x = UnicodeString("hello world\n");
+	auto y = UnicodeString("wtf is the kilometer\n");
+	UnicodeString res;
+	res += x;
+	res += y;
+	res += UnicodeString("💩💩💩");
+	res += "hihihi\n";
+	res += UnicodeSymbol('<');
+	res += '>';
+	res += "\n\n-----\n";
+	Byte smile[4] = {0xf0, 0x9f, 0x98, 0x8a};
+	res[34] = UnicodeSymbol(smile, 4);
+	res.WriteTo();
+	printf("size: %d, width: %d\n", res.Length(), res.Width());
+	// auto t = UnicodeString("💩 💩 💩\n💩");
+	// t.WriteTo();
+	// printf("| size %d, width %d\n", t.Length(), t.Width());
+	return 0;
 }
