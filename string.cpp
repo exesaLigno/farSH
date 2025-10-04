b9803c306799616176fa559d779141cd3b9a0933
Vadim Karcev <cfg888powerfull@gmail.com>
2025-10-04 23:35:32 +0300

    Added insertion and prepend methods, reworked reallocator, added some doccomments

--- string.cpp
+++ string.cpp
@@ -14,49 +14,82 @@ private:
     size_t length = 0;
     size_t width = 0;
 
-    void ReallocateBuffer(size_t multiplier = 2, size_t added_size = 0)
+    enum class ReallocationMethod
     {
+        MultiplySize,
+        AppendSize
+    };
+
+    /**
+     * @brief Reallocates internal buffer with provided method and copies all 
+     * information into new allocated buffer.
+     * @param value If method `MultiplySize` is selected, size will be multiplied 
+     * by provided `value`, if `AppendSize` is selected, size will be increased
+     * by `value`.
+     * @param method Reallocation method.
+     */
+    void ReallocateBuffer(size_t value, UnicodeString::ReallocationMethod method)
+    {
+        assert(
+            (method == ReallocationMethod::MultiplySize and value > 1) or 
+            (method == ReallocationMethod::AppendSize and value > 0)
+        );
+
         size_t old_buffer_size = bufferSize;
         UnicodeSymbol* old_buffer = buffer;
+        
+        switch (method)
+        {
+            case ReallocationMethod::AppendSize:
+                bufferSize += value;
+                break;
+            case ReallocationMethod::MultiplySize:
+                bufferSize *= value;
+                break;
+        }
 
-        bufferSize = bufferSize * multiplier + added_size;
         buffer = new UnicodeSymbol[bufferSize];
-        memcpy(buffer, old_buffer, old_buffer_size * sizeof(UnicodeSymbol));
-        delete[] old_buffer;
+
+        if (old_buffer)
+        {
+            memcpy(buffer, old_buffer, old_buffer_size * sizeof(UnicodeSymbol));
+            delete[] old_buffer;
+        }
     }
 
-    UnicodeSymbol& ReallocateAndGetFirstEmptySymbolLink()
+    inline UnicodeSymbol& GetFirstEmptySymbolLink()
     {
-        if (length >= bufferSize)
-            ReallocateBuffer();
-
         return buffer[length];
     }
 
-    void ReallocateAndInsert(const UnicodeSymbol& symbol)
+    inline UnicodeSymbol& ReallocateAndGetFirstEmptySymbolLink()
     {
         if (length >= bufferSize)
-            ReallocateBuffer();
+            ReallocateBuffer(2, ReallocationMethod::MultiplySize);
 
-        buffer[length++] = symbol;
-        width += symbol.DisplayWidth();
+        return GetFirstEmptySymbolLink();
     }
 
 public:
     UnicodeString()
     {
-        ReallocateBuffer(0, initialBufferSize);
+        ReallocateBuffer(initialBufferSize, ReallocationMethod::AppendSize);
     }
 
     UnicodeString(const char* string)
     {
-        ReallocateBuffer(0, strlen(string));
+        // Axiom (1).
+        // Due to fact that `UnicodeSymbol` consists of at least one byte, it is 
+        // general truth that `strlen(string)` is always greater or equal length 
+        // of that string converted to `UnicodeSymbol`'s. We can just allocate 
+        // space and be sure that it's enough to store provided `string`
+        ReallocateBuffer(strlen(string), ReallocationMethod::AppendSize);
 
         const Byte* string_ptr = (const Byte*) string;
 
         while (*string_ptr != 0)
         {
-            UnicodeSymbol& current = ReallocateAndGetFirstEmptySymbolLink();
+            UnicodeSymbol& current = GetFirstEmptySymbolLink();
             UnicodeSymbol::CreateInplace(current, string_ptr);
             length++;
             width += current.DisplayWidth();
@@ -78,42 +111,139 @@ public:
         width = 0;
     }
 
-    UnicodeString& operator+=(const UnicodeString& string)
+    /** 
+     * @brief Appends provided unicode string to the end of current string. If 
+     * remaining capacity is less then provided string, current string will
+     * be reallocated.
+     * @param string Unicode string to be appended to the end of current string.
+     */
+    void Append(const UnicodeString& string)
     {
+        if (length + string.length > bufferSize)
+            ReallocateBuffer(string.length, ReallocationMethod::AppendSize);
+
         for (size_t idx = 0; idx < string.length; idx++)
         {
-            UnicodeSymbol& current = ReallocateAndGetFirstEmptySymbolLink();
+            // We already allocated enought room for appending string. It is 
+            // safe to copy it without reallocations.
+            UnicodeSymbol& current = GetFirstEmptySymbolLink();
             current = string.buffer[idx];
             length++;
             width += current.DisplayWidth();
         }
-
-        return *this;
     }
 
-    UnicodeString& operator+=(const UnicodeSymbol& symbol)
+    /** 
+     * @brief Inserts provided unicode string in the position `where`. On 
+     * insertions into middle of string, remaining part will be moved to make 
+     * room for inserting string. If remaining capacity is less then provided 
+     * string, current string will be reallocated.
+     * @param where Index of element, where to start insertion. Remaining part
+     * of string (after index `where`) will be moved forward to make room for 
+     * insertion.
+     * @param string Unicode string to be inserted into current string on 
+     * specified position.
+     */
+    void Insert(int32_t where, const UnicodeString& string)
     {
-        ReallocateAndInsert(symbol);
-        return *this;
+        where = (where >= 0) ? where : length + where;
+        assert(where <= length);
+
+        if (length + string.length > bufferSize)
+            ReallocateBuffer(string.length, ReallocationMethod::AppendSize);
+
+        for (int32_t idx = length - 1; idx >= where; idx--)
+            buffer[idx + string.length] = std::move(buffer[idx]);
+
+        for (int32_t idx = 0; idx < string.length; idx++)
+            buffer[where + idx] = string.buffer[idx];
+
+        length += string.length;
+        width += string.width;
     }
 
-    UnicodeString& operator+=(const char* string)
+    /**
+     * @brief Prepends provided unicode symbol at the beggining of current 
+     * string. It reallocates current string internal buffer if it is necessary
+     * and moves all contents forward by the length of prepending string.
+     * @param string A unicode string to prepend.
+     */
+    void Prepend(const UnicodeString& string)
     {
+        Insert(0, string);
+    }
+
+    /** 
+     * @brief Appends provided unicode symbol to the end of current string. If 
+     * remaining capacity is zero, current string will be reallocated.
+     * @param symbol Symbol to be appended to the end of current string.
+     */
+    void Append(const UnicodeSymbol& symbol)
+    {
+        UnicodeSymbol& current = ReallocateAndGetFirstEmptySymbolLink();
+        current.CloneFrom(symbol);
+        length++;
+        width += current.DisplayWidth();
+    }
+
+    /**
+     * @brief Inserts provided symbol at specified position. Moves all data from
+     * index `where` to the end of string by one symbol and reallocates if it 
+     * necessary
+     * @param where Index of element, where to insert new symbol.
+     * @param symbol A unicode symbol to insert into string
+     */
+    void Insert(int32_t where, const UnicodeSymbol& symbol)
+    {
+        where = (where >= 0) ? where : length + where;
+        assert(where <= length);
+
+        if (length + 1 > bufferSize)
+            ReallocateBuffer(2, ReallocationMethod::MultiplySize);
+
+        for (int32_t idx = length - 1; idx >= where; idx--)
+            buffer[idx + 1] = std::move(buffer[idx]);
+
+        buffer[where].CloneFrom(symbol);
+
+        length++;
+        width += symbol.DisplayWidth();
+    }
+
+    /**
+     * @brief Prepends symbol at the beggining of current string. Moves all 
+     * symbols one symbol forward and reallocates buffer if it is needed.
+     * @param symbol Unicode symbol to prepend.
+     */
+    void Prepend(const UnicodeSymbol& symbol)
+    {
+        Insert(0, symbol);
+    }
+
+    void Append(const char* string)
+    {
+        size_t string_len = strlen(string);
+        if (length + string_len > bufferSize)
+            ReallocateBuffer(string_len, ReallocationMethod::AppendSize);
+
         const Byte* string_ptr = (const Byte*) string;
         while (*string_ptr != 0)
         {
-            UnicodeSymbol& current = ReallocateAndGetFirstEmptySymbolLink();
+            // We already allocated enought room for appending string (Axiom 1). 
+            // It is safe to copy it without reallocations.
+            UnicodeSymbol& current = GetFirstEmptySymbolLink();
             UnicodeSymbol::CreateInplace(current, string_ptr);
             length++;
             width += current.DisplayWidth();
         }
-        return *this;
     }
 
-    UnicodeString& operator+=(const char symbol)
+    void Append(const char symbol)
     {
-        ReallocateAndInsert(UnicodeSymbol(symbol));
-        return *this;
+        UnicodeSymbol& current = ReallocateAndGetFirstEmptySymbolLink();
+        current.CloneFrom(UnicodeSymbol(symbol));
+        length++;
+        width += current.DisplayWidth();
     }
 
     UnicodeSymbol& operator[](size_t idx)
--- testu.cpp
+++ testu.cpp
@@ -2,17 +2,20 @@
 
 int main()
 {
+	Byte smile[4] = {0xf0, 0x9f, 0x98, 0x8a};
 	auto x = UnicodeString("hello world\n");
 	auto y = UnicodeString("wtf is the kilometer\n");
 	UnicodeString res;
-	res += x;
-	res += y;
-	res += UnicodeString("💩💩💩");
-	res += "hihihi\n";
-	res += UnicodeSymbol('<');
-	res += '>';
-	res += "\n\n-----\n";
-	Byte smile[4] = {0xf0, 0x9f, 0x98, 0x8a};
+	res.Append(x);
+	res.Append(y);
+	res.Insert(8, UnicodeString("💩💩💩"));
+	res.Prepend(UnicodeString("💩💩💩"));
+	res.Prepend(UnicodeString("💩💩💩"));
+	res.Insert(3, UnicodeSymbol(smile, 4));
+	res.Append("hihihi\n");
+	res.Append(UnicodeSymbol('<'));
+	res.Append('>');
+	res.Append("\n\n-----\n");
 	res[34] = UnicodeSymbol(smile, 4);
 	res.WriteTo();
 	printf("size: %d, width: %d\n", res.Length(), res.Width());
