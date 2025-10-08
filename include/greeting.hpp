24e2e44aad5505361ef7a082aef4ad69eba60427
Vadim Karcev <cfg888powerfull@gmail.com>
2025-10-08 21:20:05 +0300

    Greeting scheme became private member

--- include/greeting.hpp
+++ include/greeting.hpp
@@ -8,26 +8,19 @@
 class Greeting
 {
 private:
+    char scheme[1000] = "{cwd} $> ";
     static constexpr int hostNameBufferSize = 1000;
     static constexpr int workDirBufferSize = 4000;
 
     static const char* GetUserName();
-
     static const char* GetHostName();
-
     static const uid_t GetUid();
-
     static const passwd* GetPwuid();
-
     static const char* GetHomeDir();
-
     static const char* GetWorkDir(bool shorten_home_dir = false, bool shorten_parential_dir_path = false, bool truncate_parential_dir_path = false);
-
     static const char* GetInfo(const char* const entry_name, size_t entry_name_size);
 
 public:
-    char scheme[1000] = "$> ";
-
     Greeting();
     Greeting(const char* _scheme);
 
