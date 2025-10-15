30d51f96d5f7733b8fcbbbdae5aa58300b69f68e
Vadim Karcev <cfg888powerfull@gmail.com>
2025-10-15 21:45:20 +0300

    Revert it! Testing syntax

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,9 +1,50 @@
 #include "shell.hpp"
+#include "syntax.hpp"
 
 int main(const int argc, const char* const argv[])
 {
-	auto f = Shell();
-	f.Run();
+	auto command = new CommandOperation();
+	auto pipe = new PipeRedirectionOperation();
+	command->AppendChild(pipe);
+	auto invocation1 = new InvocationOperation();
+	pipe->AppendChild(invocation1);
+	auto word1 = new WordOperation("cat");
+	invocation1->AppendChild(word1);
+	auto arg1 = new ArgumentOperation();
+	invocation1->AppendChild(arg1);
+	auto composition = new CompositionOperation();
+	arg1->AppendChild(composition);
+	auto environ = new EnvironmentVariableReferenceOperation();
+	composition->AppendChild(environ);
+	auto word2 = new WordOperation("USERNAME");
+	environ->AppendChild(word2);
+	auto word3 = new WordOperation("-log.txt");
+	composition->AppendChild(word3);
 
-	return 0;
+	auto file = new FileRedirectionOperation();
+	pipe->AppendChild(file);
+	auto invocation2 = new InvocationOperation();
+	file->AppendChild(invocation2);
+	auto word4 = new WordOperation("grep");
+	invocation2->AppendChild(word4);
+	auto arg2 = new ArgumentOperation();
+	invocation2->AppendChild(arg2);
+	auto arg3 = new ArgumentOperation();
+	invocation2->AppendChild(arg3);
+	auto word5 = new WordOperation("error");
+	arg2->AppendChild(word5);
+	auto raw_str = new RawStringLiteralOperation("critical warning");
+	arg3->AppendChild(raw_str);
+	auto arg4 = new ArgumentOperation();
+	file->AppendChild(arg4);
+	auto word6 = new WordOperation("errors.txt");
+	arg4->AppendChild(word6);
+
+	FILE* fd = fopen("ast.dot", "w");
+	command->DumpTo(fd);
+	fclose(fd);
+	// auto f = Shell();
+	// f.Run();
+
+	// return 0;
 }
