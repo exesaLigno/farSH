0b28a3509d7fe15f9900fcc3fdbbbb9036790d70
Vadim Karcev <cfg888powerfull@gmail.com>
2025-11-06 12:31:21 +0300

    Removed command operation

--- include/syntax.hpp
+++ include/syntax.hpp
@@ -8,7 +8,6 @@
 
 #include "syntax/operation.hpp"
 
-#include "syntax/command_operation.hpp"
 #include "syntax/composition_operation.hpp"
 #include "syntax/file_redirection_operation.hpp"
 #include "syntax/invocation_operation.hpp"
